c121651e5a1783f673c9130a589d8040bccd6b4c
Aaron Crosman <acrosman@gmail.com>
2021-06-12 13:55:56 -0400

    lint fix

--- app/preload.js
+++ app/preload.js
@@ -1,33 +1,33 @@
 // Preload script.
 const { contextBridge, ipcRenderer, remote } = require('electron');  // eslint-disable-line
-const { handlers } = require('../src/sf_calls.js');
+const { handlers } = require('../src/sf_calls');
 
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object.
 // Big hat tip: https://stackoverflow.com/a/59814127/24215.
 contextBridge.exposeInMainWorld(
   'api', {
-  send: (channel, data) => {
-    // List channels to allow.
-    const validChannels = Object.getOwnPropertyNames(handlers);
-    if (validChannels.includes(channel)) {
-      ipcRenderer.send(channel, data);
-    }
+    send: (channel, data) => {
+      // List channels to allow.
+      const validChannels = Object.getOwnPropertyNames(handlers);
+      if (validChannels.includes(channel)) {
+        ipcRenderer.send(channel, data);
+      }
+    },
+    receive: (channel, func) => {
+      const validChannels = [
+        'response_db_generated',
+        'response_login',
+        'response_logout',
+        'response_generic',
+        'response_list_objects',
+        'response_schema',
+        'log_message',
+      ];
+      if (validChannels.includes(channel)) {
+        // Remove the event to avoid information leaks.
+        ipcRenderer.on(channel, (event, ...args) => func(...args));
+      }
+    },
   },
-  receive: (channel, func) => {
-    const validChannels = [
-      'response_db_generated',
-      'response_login',
-      'response_logout',
-      'response_generic',
-      'response_list_objects',
-      'response_schema',
-      'log_message',
-    ];
-    if (validChannels.includes(channel)) {
-      // Remove the event to avoid information leaks.
-      ipcRenderer.on(channel, (event, ...args) => func(...args));
-    }
-  },
-},
 );
