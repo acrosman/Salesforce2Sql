6c412c865697c63af2d54e459d0cf8fa5bfdeb3e
Aaron Crosman <acrosman@gmail.com>
2021-06-12 13:30:56 -0400

    formating error fix

--- app/preferences-preload.js
+++ app/preferences-preload.js
@@ -6,23 +6,23 @@ const { contextBridge, ipcRenderer, remote } = require('electron');  // eslint-d
 // Big hat tip: https://stackoverflow.com/a/59814127/24215.
 contextBridge.exposeInMainWorld(
   'api', {
-  send: (channel, data) => {
-    // List channels to allow.
-    const validChannels = [
-      'preferences_save',
-    ];
-    if (validChannels.includes(channel)) {
-      ipcRenderer.send(channel, data);
-    }
+    send: (channel, data) => {
+      // List channels to allow.
+      const validChannels = [
+        'preferences_save',
+      ];
+      if (validChannels.includes(channel)) {
+        ipcRenderer.send(channel, data);
+      }
+    },
+    receive: (channel, func) => {
+      const validChannels = [
+        'preferences_confirm',
+      ];
+      if (validChannels.includes(channel)) {
+        // Remove the event to avoid information leaks.
+        ipcRenderer.on(channel, (event, ...args) => func(...args));
+      }
+    },
   },
-  receive: (channel, func) => {
-    const validChannels = [
-      'preferences_confirm',
-    ];
-    if (validChannels.includes(channel)) {
-      // Remove the event to avoid information leaks.
-      ipcRenderer.on(channel, (event, ...args) => func(...args));
-    }
-  },
-},
 );
