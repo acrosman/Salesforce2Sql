e623792d129254a3fadaed77b4e65b8d60a8d72f
Aaron Crosman <acrosman@gmail.com>
2021-06-22 19:47:49 -0400

    Adding repeat search support and logging.

--- src/find.js
+++ src/find.js
@@ -1,12 +1,50 @@
 const { BrowserWindow } = require('electron');  // eslint-disable-line
 
+let currentSearchReference;
+let currentSearchText;
+let searchEnabled = false;
+
+const enableSearch = (pageContent) => {
+  pageContent.on('found-in-page', (event, result) => {
+    pageContent.send('log_message', 'Info', `Found ${result.matches} for ${currentSearchText}`);
+    if (result.finalUpdate) {
+      pageContent.stopFindInPage('keepSelection');
+      currentSearchText = null;
+      currentSearchReference = null;
+    }
+  });
+  searchEnabled = true;
+};
+
 /**
  * executeSearch: Searches provided content for requested text.
- * @param {*} webContents: The content of the window to Search.
+ * @param {*} pageContent: The content of the window to Search.
  * @param {*} searchText: The text to search for.
  */
-const executeSearch = (webContents, searchText) => {
-  webContents.findInPage(searchText.text);
+const executeSearch = (pageContent, searchText) => {
+  if (!searchEnabled) {
+    enableSearch(pageContent);
+  }
+  // If there is no active search, or this is new text, start a search.
+  if (currentSearchText !== searchText.text || !currentSearchReference) {
+    currentSearchText = searchText.text;
+    pageContent.send('log_message', {
+      sender: 'Find',
+      channel: 'Info',
+      message: `Starting search for ${currentSearchText}`,
+    });
+    currentSearchReference = pageContent.findInPage(currentSearchText, {
+      forward: true,
+      findNext: false,
+      matchCase: true,
+    });
+  } else {
+    currentSearchReference = pageContent.findInPage(currentSearchText, {
+      forward: true,
+      findNext: true,
+      matchCase: true,
+    });
+  }
 };
 
 /**
