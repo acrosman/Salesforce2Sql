4abfd38739d1bc4ca9485b62dba5dd5fcee9f1cc
Aaron Crosman <acrosman@gmail.com>
2021-06-30 00:53:17 -0400

    Better Jest linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,15 @@ module.exports = {
     "parserOptions": {
         "ecmaVersion": 2018
     },
+    "plugins": ["jest"],
     "rules": {
         "no-unused-vars": 1,
         "no-param-reassign": ["error", { "props": false }],
         "no-underscore-dangle": 0,
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error",
     }
 };
--- src/tests/menu.test.js
+++ src/tests/menu.test.js
@@ -1,9 +1,9 @@
 const menu = require('../menu');
 
 test('Validate menu template', () => {
-  if (process.platform == 'darwin') {
-    expect(menu.menuTemplate.length).toBe(4);
+  if (process.platform === 'darwin') {
+    expect(menu.menuTemplate).toHaveLength(4);
   } else {
-    expect(menu.menuTemplate.length).toBe(3);
+    expect(menu.menuTemplate).toHaveLength(3);
   }
 });
