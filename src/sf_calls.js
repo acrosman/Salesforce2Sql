5857234db3d4701700dc6c439d15f8fd2537e07b
Aaron Crosman <acrosman@gmail.com>
2022-01-12 23:54:13 -0500

    add user, improve messages

--- src/sf_calls.js
+++ src/sf_calls.js
@@ -53,6 +53,7 @@ const standardObjectsByNamespace = {
     'OpportunityContactRole',
     'RecordType',
     'Task',
+    'User',
   ],
   eda: [
     'Account',
@@ -64,6 +65,7 @@ const standardObjectsByNamespace = {
     'Lead',
     'RecordType',
     'Task',
+    'User',
   ],
   other: [
     'Account',
@@ -81,6 +83,7 @@ const standardObjectsByNamespace = {
     'Product2',
     'RecordType',
     'Task',
+    'User',
   ],
 };
 
@@ -518,6 +521,8 @@ const buildDatabase = (settings) => {
           message: 'Database created',
           responses: tableStatuses,
         });
+      } else {
+        mainWindow.webContents.send('update_loader', { message: `Creating ${tables.length} tables, ${Object.getOwnPropertyNames(tableStatuses).length} complete` });
       }
     })
     .catch((err) => {
