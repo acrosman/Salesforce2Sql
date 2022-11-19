7fdc4aca13880e1a7fc305e16232376d90d61f32
Aaron Crosman <acrosman@gmail.com>
2022-11-19 12:39:52 -0500

    Initial Electron Forge Setup

---
+++ forge.config.js
@@ -0,0 +1,53 @@
+const { utils: { fromBuildIdentifier } } = require('@electron-forge/core');
+
+module.exports = {
+  buildIdentifier: process.env.IS_BETA ? 'beta' : 'prod',
+  packagerConfig: {
+    appBundleId: fromBuildIdentifier({ beta: 'com.beta.acrosman.Salesforce2Sql', prod: 'com.Salesforce2Sql' }),
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: 'salesforce2Sql',
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin', 'linux'],
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {
+        format: 'ULFO',
+        overwrite: true,
+      },
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-electronegativity',
+      config: {
+        isSarif: true,
+      },
+    },
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'acrosman',
+          name: 'Salesforce2Sql',
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
+};
